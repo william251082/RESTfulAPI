c01f7bf3c8abfbb2115d511e0a6496be91772768
William del Rosario <williamdelrosario@yahoo.com>
2018-03-17 08:17:05 +0100

    Implementing the relationships between models

--- app/Buyer.php
+++ app/Buyer.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Transaction;
 
 class Buyer extends User
 {
-    //
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
