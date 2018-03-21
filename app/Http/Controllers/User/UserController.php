58f55cc0dd7d604787b6d8cde11a24661d58dc83
William del Rosario <williamdelrosario@yahoo.com>
2018-03-21 11:40:45 +0100

    Using laravel implicit model bindings for sme methods

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -57,11 +57,9 @@ class UserController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(User $user)
     {
-        $user = User::findOrFail($id);
-
-        return $this->showOne($user, 201);
+        return $this->showOne($user);
     }
 
     /**
@@ -116,9 +114,8 @@ class UserController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        $user = User::findOrFail($id);
 
         $user->delete();
 
