d0c6d39b6f7f1f0c7ccbd5d443c1a083b773e73c
William del Rosario <williamdelrosario@yahoo.com>
2018-03-21 12:07:55 +0100

    Modifying the migrations for soft deleting

--- app/Seller.php
+++ app/Seller.php
@@ -2,9 +2,18 @@
 
 namespace App;
 
+use App\Product;
+use App\Scopes\SellerScope;
 
 class Seller extends User
 {
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new SellerScope());
+    }
+
     public function products()
     {
         return $this->hasMany(Product::class);
