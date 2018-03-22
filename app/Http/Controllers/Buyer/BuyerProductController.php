8cd5ead48d4c77e701cd5c4faa7a946535a51f86
William del Rosario <williamdelrosario@yahoo.com>
2018-03-22 11:53:56 +0100

    Implementing the index Method for buyer transaction controller2

---
+++ app/Http/Controllers/Buyer/BuyerProductController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Buyer;
+
+use App\Buyer;
+use App\Http\Controllers\ApiController;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class BuyerProductController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Buyer $buyer)
+    {
+        $products = $buyer->transactions()->with('products')->get();
+
+        return $this->showAll($products);
+    }
+}
