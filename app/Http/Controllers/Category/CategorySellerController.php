d1ebf9fff92751317566467c5adba462b65e4119
William del Rosario <williamdelrosario@yahoo.com>
2018-03-22 13:18:38 +0100

    Implementing the index Method for category seller controller

---
+++ app/Http/Controllers/Category/CategorySellerController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Category;
+
+use App\Category;
+use App\Http\Controllers\ApiController;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CategorySellerController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Category $category)
+    {
+        $sellers = $category->products()
+            ->with('seller')
+            ->get()
+            ->pluck('seller')
+            ->unique()
+            ->values();
+
+        return $this->showAll($sellers);
+    }
+}
