ca03bfd766301f337742abb1d2c70f8324acb929
William del Rosario <williamdelrosario@yahoo.com>
2018-03-22 13:11:46 +0100

    Implementing the index Method for category product controller

---
+++ app/Http/Controllers/Category/CategoryProductController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Category;
+
+use App\Category;
+use App\Http\Controllers\ApiController;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CategoryProductController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Category $category)
+    {
+        $products = $category->products;
+
+        return $this->showAll($products);
+    }
+}
